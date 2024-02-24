5fbe98c306a8f3d937d882b8dcf64de7e484c8c7
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-24 15:08:46 +0100

    Updated code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 $filename = '#   IdentityFile ~\/.ssh\/id_rsa'
 $edit = '   IdentityFile ~\/.ssh\/school'
 
-exec { 'configure-ssh_config':
+exec { 'turn-off_password':
   command => "sed -i 's/#    PasswordAuthentication yes/    PasswordAuthentication no/' /etc/ssh/ssh_config",
   path    => ['/bin', '/usr/bin'],
   onlyif  => "grep -q '#    PasswordAuthentication no' /etc/ssh/ssh_config"
 }
 
-exec { 'configure-ssh_config':
+exec { 'set_key-file':
   command => "sed -i 's/${filename}/${edit}/' /etc/ssh/ssh_config",
   path    => ['/bin', '/usr/bin'],
   onlyif  => "grep -q '#   IdentityFile ~/.ssh/id_rsa' /etc/ssh/ssh_config"
