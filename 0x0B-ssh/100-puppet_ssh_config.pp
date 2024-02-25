4275844a599e61961144fa757c22f2deae6de05d
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-25 12:14:16 +0100

    Fixed up code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,14 @@
-#edit config file
-
-$filename = '#   IdentityFile ~\/.ssh\/id_rsa'
-$edit = '   IdentityFile ~\/.ssh\/school'
-
-exec { 'turn-off_password':
-  command => "sed -i 's/#    PasswordAuthentication yes/    PasswordAuthentication no/' /etc/ssh/ssh_config",
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => "grep -q '#    PasswordAuthentication no' /etc/ssh/ssh_config"
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^#    PasswordAuthentication no',
 }
 
-exec { 'set_key-file':
-  command => "sed -i 's/${filename}/${edit}/' /etc/ssh/ssh_config",
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => "grep -q '#   IdentityFile ~/.ssh/id_rsa' /etc/ssh/ssh_config"
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^#   IdentityFile ~/.ssh/id_rsa',
 }
