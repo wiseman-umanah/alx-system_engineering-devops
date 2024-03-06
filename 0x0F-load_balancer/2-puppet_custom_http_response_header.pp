b0b32a01ff521fa0ba5527497c7f394a5966a1e4
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-06 07:30:31 +0100

    Updated code with correct work

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,31 @@
-#install and config HAproxy
-exec { 'Get Updates':
-  command  => 'apt-get update',
-  provider => 'shell',
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
-exec { 'Install nginx':
-  command  => 'apt install -y nginx',
-  provider => 'shell',
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-file_line { 'Configure nginx':
+file_line { 'a':
   ensure  => 'present',
-  path    => '/etc/nginx/sites_available/default',
+  path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => '\nadd_header X-Served-By $HOSTNAME;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
   require => Package['nginx'],
 }
 
