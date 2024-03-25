eb65f55badaeada86dab74008f0c9f6e9205eebd
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-25 15:17:49 +0100

    Updated code block to block outer import

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Module to display for a given employee ID,
 returns information about his/her TODO list progress."""
-import requests
-from sys import argv
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-
-link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-res = requests.get(link)
-task = requests.get(f"{link}/todos")
-employeeName = res.json().get("name")
-todos = task.json()
-completed = []
-for i in todos:
-    if i["completed"] is True:
-        completed.append(i["title"])
-string = f"Employee {employeeName} is done with tasks"
-print(f"{string}({len(completed)}/{len(todos)}):")
-for i in completed:
-    print("\t ", i)
+    link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    res = requests.get(link)
+    task = requests.get(f"{link}/todos")
+    employeeName = res.json().get("name")
+    todos = task.json()
+    completed = []
+    for i in todos:
+        if i["completed"] is True:
+            completed.append(i["title"])
+    string = f"Employee {employeeName} is done with tasks"
+    print(f"{string}({len(completed)}/{len(todos)}):")
+    for i in completed:
+        print("\t ", i)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """Module to export employee's details
 to be save to JSON"""
-import requests
-import json
-from sys import argv
+if __name__ == "__main__":
+    import requests
+    import json
+    from sys import argv
 
+    link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    res = requests.get(link)
+    task = requests.get(f"{link}/todos")
+    employeeName = res.json().get("username")
+    todos = task.json()
+    employID = {}
+    temp = []
+    for i in todos:
+        employtask = {}
+        employtask["task"] = i["title"]
+        employtask["completed"] = i["completed"]
+        employtask["username"] = employeeName
+        temp.append(employtask)
+    employID[argv[1]] = temp
 
-link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-res = requests.get(link)
-task = requests.get(f"{link}/todos")
-employeeName = res.json().get("username")
-todos = task.json()
-employID = {}
-temp = []
-for i in todos:
-    employtask = {}
-    employtask["task"] = i["title"]
-    employtask["completed"] = i["completed"]
-    employtask["username"] = employeeName
-    temp.append(employtask)
-employID[argv[1]] = temp
-
-with open(f"{argv[1]}.json", "w") as json_file:
-    json_obj = json.dumps(employID)
-    json_file.write(json_obj)
+    with open(f"{argv[1]}.json", "w") as json_file:
+        json_obj = json.dumps(employID)
+        json_file.write(json_obj)
