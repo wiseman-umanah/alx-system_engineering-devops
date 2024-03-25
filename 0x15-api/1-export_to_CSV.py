5ae7e0d6409e223c8cec5a5a0be5eaef0d3a7890
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-25 15:06:51 +0100

    Started new project on APIs

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to save information about
+employee in csv file"""
+import requests
+import csv
+from sys import argv
+
+
+link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+res = requests.get(link)
+task = requests.get(f"{link}/todos")
+employeeName = res.json().get("username")
+todos = task.json()
+with open(f"{argv[1]}.csv", "w") as csv_file:
+    csv_write = csv.writer(csv_file)
+    for i in todos:
+        csv_write.writerow([argv[1], employeeName, i["completed"], i["title"]])
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to export employee's details
+to be save to JSON"""
+import requests
+import json
+
+
+employID = {}
+link = f"https://jsonplaceholder.typicode.com/users"
+res = requests.get(link).json()
+for employee in res:
+    employeeName = employee.get("username")
+    task = requests.get(f"{link}/{employee.get('id')}/todos")
+    todos = task.json()
+    temp = []
+    for i in todos:
+        employtask = {}
+        employtask["task"] = i["title"]
+        employtask["completed"] = i["completed"]
+        employtask["username"] = employeeName
+        temp.append(employtask)
+    employID[employee.get('id')] = temp
+
+with open(f"todo_all_employees.json", "w") as json_file:
+    json_obj = json.dumps(employID)
+    json_file.write(json_obj)
