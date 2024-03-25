e51f1fb5b310c12a5a70e97b04677d8a9082e5f3
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-25 16:18:55 +0100

    this should work

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,11 +10,17 @@ if __name__ == "__main__":
     task = requests.get(f"{link}/todos")
     employeeName = res.json().get("name")
     todos = task.json()
+    num_todos = len(todos)
     completed = []
     for i in todos:
         if i["completed"] is True:
             completed.append(i["title"])
-    string = f"Employee {employeeName} is done with tasks"
-    print(f"{string}({len(completed)}/{len(todos)}):")
-    for i in completed:
-        print("\t", i)
+    num_com = len(completed)
+    with open("student_output", "w") as file:
+        string = f"Employee {employeeName} is done with tasks"
+        file.write(f"{string}({num_com}/{num_todos}):\n")
+        print(f"{string}({num_com}/{num_todos}):")
+    with open("student_output", "a") as file:
+        for i in completed:
+            file.write("\t " + i + "\n")
+            print("\t", i)
