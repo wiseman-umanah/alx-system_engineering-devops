2f1d20712fc3e9c184353ad7c50e8715a14ac50e
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-25 16:20:10 +0100

    this should work

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Module to display for a given employee ID,
-returns information about his/her TODO list progress."""
-if __name__ == "__main__":
-    import requests
-    from sys import argv
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
-    link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    res = requests.get(link)
-    task = requests.get(f"{link}/todos")
-    employeeName = res.json().get("name")
-    todos = task.json()
-    num_todos = len(todos)
-    completed = []
-    for i in todos:
-        if i["completed"] is True:
-            completed.append(i["title"])
-    num_com = len(completed)
-    with open("student_output", "w") as file:
-        string = f"Employee {employeeName} is done with tasks"
-        file.write(f"{string}({num_com}/{num_todos}):\n")
-        print(f"{string}({num_com}/{num_todos}):")
-    with open("student_output", "a") as file:
-        for i in completed:
-            file.write("\t " + i + "\n")
-            print("\t", i)
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
