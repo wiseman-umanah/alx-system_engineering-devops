b1be0cea720724cec3f4d8b7634728e3872c6d71
wiseman-umanah <wisemanumanah@gmail.com>
2024-04-16 00:04:34 +0100

    changed open file permit for user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i s/ nofile .*/ nofile unlimited/g /etc/security/limits.conf',
+  command => 'sed -i "s/holberton.*/holberton nofile unlimited/g" /etc/security/limits.conf',
   path    => ['/bin', 'usr/bin', 'usr/sbin']
 }
 
